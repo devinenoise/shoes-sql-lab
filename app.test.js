2a420a0153fe474df454ea45341d647a96d28c79
Kyle Devine <devinenoise@gmail.com>
2020-02-21 08:58:09 -0800

    backend functionality

---
+++ app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+
+describe('GET API/SHOES ', () => {
+    test('It should respond with correctly formatted data', async (done) => {
+        const response = await request(`https://rocky-basin-80195.herokuapp.com/`)
+            .get('/api/shoes');
+
+        expect(response.body).toEqual({
+            name: expect.any(String),
+            type: expect.any(String),
+            url: expect.any(String),
+            brand: expect.any(String),
+            laces: expect.any(String)
+        });
+        expect(response.statusCode).toBe(200);
+
+        done();
+    });
+});
+
--- data/create-tables.js
+++ data/create-tables.js
@@ -16,7 +16,7 @@ async function run() {
     try {
         // initiate connecting to db
         await client.connect();
-    
+
         // run a query to create tables
         await client.query(`
             CREATE TABLE shoes (
@@ -39,5 +39,5 @@ async function run() {
         // success or failure, need to close the db connection
         client.end();
     }
-    
+
 }
