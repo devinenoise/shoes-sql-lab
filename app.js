832eee1ac3adab571873809514ddc7c56ea9f34a
Kyle Devine <devinenoise@gmail.com>
2020-02-25 08:46:17 -0800

    update route working

--- app.js
+++ app.js
@@ -63,14 +63,7 @@ app.put('/api/shoes', async (req, res) => {
     // using req.body instead of req.params or req.query (which belong to /GET requests)
     try {
         // make a new shoe out of the shoe that comes in req.body;
-        console.log(req.body);
-        console.log(`UPDATE shoes
-        SET name='${req.body.name}', 
-            laces=${req.body.laces}, 
-            brand='${req.body.brand}', 
-            type_id=${req.body.typeId}, 
-            url='${req.body.url}'
-        WHERE id=${req.body.id};`)
+        
         const result = await client.query(`
             UPDATE shoes
             SET name='${req.body.name}', 
